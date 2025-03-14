51ad3db55afb7ef6ca2230019f4c255a51031548
Drodriguezponce1 <Danieljrod98@yahoo.com>
2025-03-14 00:54:11 -0700

    small update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,16 @@
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
-import App from './App.jsx'
-
-import { ChakraProvider } from '@chakra-ui/react'
-
-createRoot(document.getElementById('root')).render(
-
-  <StrictMode>
-
-    <ChakraProvider>
-
-      <App />
-
-    </ChakraProvider>
-
-  </StrictMode>
-
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+	  <BrowserRouter>
+			  <ChakraProvider>
+				  <App />
+			  </ChakraProvider>
+	  </BrowserRouter>
+	</React.StrictMode>
+);
