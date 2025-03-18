50021a02174d6de5c99b2a263f4d34ade59cf2ad
Drodriguezponce1 <Danieljrod98@yahoo.com>
2025-03-17 21:24:18 -0700

    changes to CreateMusic

--- frontend/src/pages/CreateMusic.jsx
+++ frontend/src/pages/CreateMusic.jsx
@@ -1,103 +1,68 @@
 import React, { useState } from 'react'
-import { Container, VStack, Heading, Box, useColorModeValue, Select, Input } from "@chakra-ui/react"
+import { Container, VStack, Heading, Box, useColorModeValue, Input } from "@chakra-ui/react"
 
 const CreateMusic = () => {
-    const [newProduct, setNewProduct] = useState({
+    const [newMusic, setNewMusic] = useState({
         name: "",
+        artist: "",
+        genre: "",
+        year: "",
         price: "",
+        format: "",
         image: "",
     });
-
-    const [selectedCategory, setSelectedCategory] = useState("");
-
-    const handleCategoryChange = (e) => {
-        setSelectedCategory(e.target.value);
-    };
 
     return (
         <Container maxW="container.sm">
             <VStack spacing={8}>
-                <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>Create New Product</Heading>
-                <Select size="sm" width="320px" onChange={handleCategoryChange}>
-                    {collection.map((item, index) => (
-                        <option key={index} value={item.label}>{item.label}</option>
-                    ))}
-                </Select>
-
+                <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>Create Music Resource</Heading>
+               
                 <Box w={"full"} bg={useColorModeValue("white", "red.800")} p={6} rounded={"lg"} shadow={"md"}>
                     <VStack spacing={4}>
                         <Input
-                            placeholder='Name'
+                            placeholder='Name (Ex: Album Name)'
                             name='name'
-                            value={newProduct.name}
-                            onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}
+                            value={newMusic.name}
+                            onChange={(e) => setNewMusic({ ...newMusic, name: e.target.value })}
+                        />
+                        <Input
+                            placeholder='Artist'
+                            name='artist'
+                            value={newMusic.artist}
+                            onChange={(e) => setNewMusic({ ...newMusic, artist: e.target.value })}
+                        />
+                        <Input
+                            placeholder='Genre'
+                            name='genre'
+                            value={newMusic.genre}
+                            onChange={(e) => setNewMusic({ ...newMusic, genre: e.target.value })}
+                        />
+                        <Input
+                            placeholder='Year'
+                            name='year'
+                            type='number'
+                            value={newMusic.year}
+                            onChange={(e) => setNewMusic({ ...newMusic, year: e.target.value })}
                         />
                         <Input
                             placeholder='Price'
                             name='price'
                             type='number'
-                            value={newProduct.price}
-                            onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value })}
+                            value={newMusic.price}
+                            onChange={(e) => setNewMusic({ ...newMusic, price: e.target.value })}
+                        />
+                        <Input
+                            placeholder='Format (Ex: CD, Vinyl, Digital)'
+                            name='format'
+                            value={newMusic.format}
+                            onChange={(e) => setNewMusic({ ...newMusic, format: e.target.value })}
                         />
                         <Input
                             placeholder='Image URL'
                             name='image'
-                            value={newProduct.image}
-                            onChange={(e) => setNewProduct({ ...newProduct, image: e.target.value })}
-                        />
-                        <Input
-                            placeholder='Year'
-                            name='year'
-                            value={newProduct.year}
-                            onChange={(e) => setNewProduct({ ...newProduct, year: e.target.value })}
-                        />
-                        
-                        {selectedCategory === "Music" && (
-                            <Box w={"full"} bg={useColorModeValue("white", "red.800")} p={6} rounded={"lg"} shadow={"md"}>
-                                <Input
-                                    placeholder='Artist'
-                                    name='artist'
-                                    value={newProduct.artist || ""}
-                                    onChange={(e) => setNewProduct({ ...newProduct, artist: e.target.value })}
-                                />
-                                <Input
-                                    placeholder='Genre'
-                                    name='genre'
-                                    value={newProduct.artist || ""}
-                                    onChange={(e) => setNewProduct({ ...newProduct, genre: e.target.value })}
-                                />
-                                <Input
-                                    placeholder='Genre'
-                                    name='artist'
-                                    value={newProduct.artist || ""}
-                                    onChange={(e) => setNewProduct({ ...newProduct, artist: e.target.value })}
-                                />
-                            </Box>
-                        )}
-                        {selectedCategory === "Manga" && (
-                            <Input
-                                placeholder='Author'
-                                name='author'
-                                value={newProduct.author || ""}
-                                onChange={(e) => setNewProduct({ ...newProduct, author: e.target.value })}
-                            />
-                        )}
-                        {selectedCategory === "Trading Cards" && (
-                            <Input
-                                placeholder='Card Type'
-                                name='cardType'
-                                value={newProduct.cardType || ""}
-                                onChange={(e) => setNewProduct({ ...newProduct, cardType: e.target.value })}
-                            />
-                        )}
-                        {selectedCategory === "Video Games" && (
-                            <Input
-                                placeholder='Platform'
-                                name='platform'
-                                value={newProduct.platform || ""}
-                                onChange={(e) => setNewProduct({ ...newProduct, platform: e.target.value })}
-                            />
-                        )}
+                            value={newMusic.image}
+                            onChange={(e) => setNewMusic({ ...newMusic, image: e.target.value })}
+                        /> 
                     </VStack>
                 </Box>
             </VStack>
@@ -105,11 +70,5 @@ const CreateMusic = () => {
     )
 }
 
-const collection = [
-    { label: "Music" },
-    { label: "Manga" },
-    { label: "Trading Cards" },
-    { label: "Video Games" },
-]
 
 export default CreateMusic
